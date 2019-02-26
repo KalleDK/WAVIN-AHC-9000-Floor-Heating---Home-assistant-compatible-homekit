5be4764d60b1122fd82d7bad1198a23f9317b342
jascdk <35434108+jascdk@users.noreply.github.com>
2019-02-26 20:18:42 +0100

    Add files via upload

---
+++ src/PrivateConfig.h
@@ -0,0 +1,10 @@
+#include <ESP8266WiFi.h>
+
+const String   WIFI_SSID = "XXXXXXXXXXXXXXXXXXX";                // wifi ssid
+const String   WIFI_PASS = "XXXXXXXXX";                          // wifi password
+
+const String   MQTT_SERVER = "XXXXXXXX"; // mqtt server
+//const String   MQTT_USER   = "Enter mqtt username here";       // mqtt user
+//const String   MQTT_PASS   = "Enter mqtt password here";       // mqtt password
+
+// Uncomment the strings above, if you use pass and username in the MQTT broker (remember to uncomment the correct lines 286 and 287 in main.cpp code).
