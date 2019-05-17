038e138d6fc6e525cd42a0ad7346207c013c82e7
jascdk <35434108+jascdk@users.noreply.github.com>
2019-05-17 20:16:41 +0200

    Update PrivateConfig.h

--- src/PrivateConfig.h
+++ src/PrivateConfig.h
@@ -1,10 +1,8 @@
 #include <ESP8266WiFi.h>
 
-const String   WIFI_SSID = "XXXXXXXXXXXXXXXXXXX";                // wifi ssid
-const String   WIFI_PASS = "XXXXXXXXX";                          // wifi password
+const String   WIFI_SSID = "Enter wireless SSID here";         // wifi ssid
+const String   WIFI_PASS = "Enter wireless password here";     // wifi password
 
-const String   MQTT_SERVER = "XXXXXXXX"; // mqtt server
-//const String   MQTT_USER   = "Enter mqtt username here";       // mqtt user
-//const String   MQTT_PASS   = "Enter mqtt password here";       // mqtt password
-
-// Uncomment the strings above, if you use pass and username in the MQTT broker (remember to uncomment the correct lines 286 and 287 in main.cpp code).
+const String   MQTT_SERVER = "Enter mqtt server address here"; // mqtt server address without port number
+const String   MQTT_USER   = "Enter mqtt username here";       // mqtt user. Use "" for no username
+const String   MQTT_PASS   = "Enter mqtt password here";       // mqtt password. Use "" for no password
