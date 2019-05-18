fa1298e2fe4998311d2e06d8116ec7aba8c95ed6
Jacob Scherrebeck <jacobscherrebeck@JacobsiMac1025.lan>
2019-05-18 21:47:27 +0200

    -

--- src/PrivateConfig.h
+++ src/PrivateConfig.h
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 
-const String   WIFI_SSID = "Enter wireless SSID here";         // wifi ssid
-const String   WIFI_PASS = "Enter wireless password here";     // wifi password
+const String   WIFI_SSID = "write here";         // wifi ssid
+const String   WIFI_PASS = "write here";     // wifi password
 
-const String   MQTT_SERVER = "Enter mqtt server address here"; // mqtt server address without port number
-const String   MQTT_USER   = "Enter mqtt username here";       // mqtt user. Use "" for no username
-const String   MQTT_PASS   = "Enter mqtt password here";       // mqtt password. Use "" for no password
+const String   MQTT_SERVER = "write here"; // mqtt server address without port number
+const String   MQTT_USER   = "write here";       // mqtt user. Use "" for no username
+const String   MQTT_PASS   = "write here";       // mqtt password. Use "" for no password
